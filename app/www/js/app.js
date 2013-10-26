7ca77be66587fc81c05166591f55ed0f57664b28
bignis <bignis@gmail.com>
2013-10-26 16:19:31 -0500

    Two screens now

--- app/www/js/app.js
+++ app/www/js/app.js
@@ -12,7 +12,13 @@ angulargap.controller("PrepopulatedPageController", function ($scope) {
             $scope.title = "Minneapolis Sculpture Garden";
             $scope.locationCopy = "The Minneapolis Sculpture Garden is one of the crown jewels of the city’s park system, uniting two of Minnesota’s most cherished resources—its greenspace and its cultural life. The 11-acre site Directions, home to the iconic Spoonbridge and Cherry, showcases more than 40 works from the Walker Art Center’s renowned collection. A partnership with the Minneapolis Park & Recreation Board, the Garden is a top destination for visitors, who enjoy not only the art but also the seasonal displays in the Cowles Conservatoryi and the Alene Grossman Memorial Arbor and Flower Garden.";
             $scope.stockPhotoUrl = "img/CherryStockPhoto.png";
-            $scope.nextPageId = "foobar";
+
+            $scope.fullScreenImageUrl = "img/MSG-spoon-completed.png"
+            $scope.nextPageId = "fish";
+            break;
+        case "fish":
+            $scope.fullScreenImageUrl = "img/MSG-fish-completed.png"
+            $scope.nextPageId = "fish";
             break;
         default:
             alert("no id parameter found on the url!");
