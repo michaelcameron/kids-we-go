0718cc0d053d1e0a4c8d91e12e264d519e7ea3f4
Michael Cameron <michael.e.cameron@gmail.com>
2013-10-26 19:51:13 -0500

    Remove status bar from ios launch (along with other aritfacts not in source)

--- app/www/js/index.js
+++ app/www/js/index.js
@@ -35,7 +35,7 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        console.log("onDeviceReady")
+        StatusBar.hide();
         app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
